ee2ffb111ada992eb0d0e172bdd9d37a77f35fa1
Manu-Andaluz <manupeandaluz@gmail.com>
2022-08-10 19:42:44 -0300

    Adding username to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:manu-andaluz./frequent-questions-app.git main:gh-pages
+# git push -f git@github.com:Manu-Andaluz./frequent-questions-app.git main:gh-pages
 
 cd -
